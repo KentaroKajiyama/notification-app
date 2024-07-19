35551fab14655bfb34bd5b5969c90476981e33f1
KentaroKajiyama <kemushi555@icloud.com>
2024-07-19 23:51:15 +0900

    fix-backend: app層、connectionの削除機能について非同期に修正。

--- packages/backend/app/hospital/remove-connection.ts
+++ packages/backend/app/hospital/remove-connection.ts
@@ -4,16 +4,20 @@ import { inject, injectable } from "inversify";
 import { TYPES } from "../../di/types.js";
 
 export interface IRemoveConnectionUseCase {
-  execute(id: HospitalId): void;
+  execute(id: HospitalId): Promise<void>;
 }
 @injectable()
 export class RemoveConnectionUseCaseImpl implements IRemoveConnectionUseCase {
   private _connectionManagerRepository: IConnectionManagerRepository;
-
   constructor(@inject(TYPES.IConnectionManagerRepository) connectionManagerRepository: IConnectionManagerRepository) {
     this._connectionManagerRepository = connectionManagerRepository;
   }
-  execute(id: HospitalId) {
-    this._connectionManagerRepository.removeConnection(id);
+  async execute(id: HospitalId): Promise<void> {
+    try{
+      this._connectionManagerRepository.removeConnection(id);
+    } catch(error){
+      console.log(error);
+      throw new Error("There is an error for removing connection.")
+    }
   }
 }
