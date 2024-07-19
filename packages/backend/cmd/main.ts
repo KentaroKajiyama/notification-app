c34cb43533af3e9c06caa6aaa2cb3004fd1262fe
KentaroKajiyama <kemushi555@icloud.com>
2024-07-19 22:37:37 +0900

    add-backend: entry point の実装

--- packages/backend/cmd/main.ts
+++ packages/backend/cmd/main.ts
@@ -1,12 +1,24 @@
 import express from "express";
+import { CheckinHandler } from "../presentation/patient/handler.js";
+import { ConnectionHandler } from "../presentation/hospital/handler.js";
+import { ServerSentEventsHandler } from "../presentation/hospital/handler.js";
+import { checkinContainer, connectionContainer, sseContainer } from "../di/inversify.config.js";
+import { TYPES } from "../di/types.js";
+
 
 const app = express();
 const port = 3000;
+const checkinHandler = checkinContainer.get<CheckinHandler>(TYPES.PatientHandler);
+const connectionHandler = connectionContainer.get<ConnectionHandler>(TYPES.ConnectionHandler);
+const sseHandler = sseContainer.get<ServerSentEventsHandler>(TYPES.ServerSentEventsHandler);
 
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello World!" });
-});
+app.use(express.json());
+
+app.post("/api/v1/patient/checkin", checkinHandler.execute);
+app.post("/api/v1/hospital/connection", connectionHandler.execute);
+app.post("/api/v1/hospital/sse",sseHandler.execute);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
