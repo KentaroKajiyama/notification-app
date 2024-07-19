f5d97907c7bd2cff04317dfe7fa78c36d84cb5dd
KentaroKajiyama <kemushi555@icloud.com>
2024-07-20 00:41:42 +0900

    fix-backend: 引き取るdata型の変更

--- packages/backend/app/hospital/remove-connection.ts
+++ packages/backend/app/hospital/remove-connection.ts
@@ -1,10 +1,10 @@
 import { IConnectionManagerRepository } from "../../domain/repository/connection-manager.js";
-import { HospitalId } from "../../domain/value-object/id.vo.js";
+import { createHospitalId } from "../../domain/value-object/id.vo.js";
 import { inject, injectable } from "inversify";
 import { TYPES } from "../../di/types.js";
 
 export interface IRemoveConnectionUseCase {
-  execute(id: HospitalId): Promise<void>;
+  execute(id: string): Promise<void>;
 }
 @injectable()
 export class RemoveConnectionUseCaseImpl implements IRemoveConnectionUseCase {
@@ -12,9 +12,10 @@ export class RemoveConnectionUseCaseImpl implements IRemoveConnectionUseCase {
   constructor(@inject(TYPES.IConnectionManagerRepository) connectionManagerRepository: IConnectionManagerRepository) {
     this._connectionManagerRepository = connectionManagerRepository;
   }
-  async execute(id: HospitalId): Promise<void> {
+  async execute(id: string): Promise<void> {
     try{
-      this._connectionManagerRepository.removeConnection(id);
+      const hospital_id = createHospitalId(id)
+      this._connectionManagerRepository.removeConnection(hospital_id);
     } catch(error){
       console.log(error);
       throw new Error("There is an error for removing connection.")
