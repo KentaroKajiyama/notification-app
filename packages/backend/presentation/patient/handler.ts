c0940da74b74f1ec4044f6e01dad0d758ff57435
KentaroKajiyama <kemushi555@icloud.com>
2024-07-19 22:36:42 +0900

    add-backend: presentation層、患者のcheckinに対するハンドリングの実装。

---
+++ packages/backend/presentation/patient/handler.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { ISearchPatientUseCase } from "../../app/patient/search.js";
+import { validationResult } from "express-validator";
+import axios from "axios";
+import { inject, injectable } from "inversify";
+import { TYPES } from "../../di/types.js";
+import "reflect-metadata";
+
+@injectable()
+export class CheckinHandler {
+  private _searchPatientUseCase: ISearchPatientUseCase;
+  constructor(@inject(TYPES.ISearchPatientUseCase) searchPatientUseCase: ISearchPatientUseCase) {
+    this._searchPatientUseCase = searchPatientUseCase;
+  }
+  async execute(req: Request, res: Response):Promise<any>{
+    try{
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { patient_id, hospital_id } = req.body;
+      const patient = await this._searchPatientUseCase.execute(patient_id);
+      await axios.post("http://localhost:3000/api/v1/hospital/sse",{patient: JSON.stringify(patient), hospital_id: hospital_id});
+      res.status(200).json({ message: "Check-in successful" });
+    } catch(err){
+      console.error(err);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+}
